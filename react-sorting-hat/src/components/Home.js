4f8d85fc9cc7c90bd9198215fefb6a731b5c3667
openwell <timileyinojo@yahoo.com>
2019-06-01 17:20:30 +0100

    app setup

---
+++ react-sorting-hat/src/components/Home.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+export class questions extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Hogwarts School of Witchcraft and Wizardry</h1>
+            </div>
+        )
+    }
+}
+
+export default questions
+// Gryffindor  strong, bravery and leadership  loyal, courageous daring, nerve, and chivalry
+// lion and its colours are scarlet and gold.
+// Sir Nicholas de Mimsy-Porpington, also known as "Nearly Headless Nick", was the house ghost.
+// element of fire
+
+
+// Hufflepuff   modest, gentle and kind  hard work, dedication, patience, loyalty, and fair play.
+// emblematic animal is the badger, and Yellow and Black are its colours
+//  Fat Friar is its ghost. 
+// element of earth
+// Newt Scamander belonged to what house
+
+
+// Ravenclaw  intelligence  knowledge, and wit. 
+// emblematic animal is the eagle, and its colours are blue and bronze.
+// ghost of Ravenclaw is the Grey Lady
+//element of air.
+// Ravenclaw students must answer a riddle as opposed to giving a password to enter their dormitories
+
+
+// Slytherin   clever and ambitious cunning and resourcefulness
+//emblematic animal is the serpent
+//  Bloody Baron is the house ghost. 
+// element Water
+
+
+
+// is the sorting hat song different every year
+// what is the game played every year by the students of Hogwart  - Inter-House Quidditch Cup.
+// what is hogwart famous hall called -- great
+// Tom Riddle and Snape were Half-blood 
+// ginny weasley
+//sirius black  ===harry god father
+//Common room
+// Gryffindor Tower
+// Hufflepuff Basement
+// Ravenclaw Tower
+// Slytherin Dungeon
